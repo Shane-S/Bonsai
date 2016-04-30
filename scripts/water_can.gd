97b1b447e18c4259bc44e5283db14be29d3cc3cc
brian <brian@copypants.com>
2016-04-30 16:33:22 -0700

    added visual for watering can

--- scripts/water_can.gd
+++ scripts/water_can.gd
@@ -5,9 +5,12 @@ signal watering_can_clicked
 
 export(int) var water_amount = 3
 
+onready var water_anim = get_node("water/AnimationPlayer")
+
 func _on_Panel_input_event(evt):
 	#emit the signal on mouse-up.
 	#you can listen for the signal by using the connection editor
 	if (evt.type == InputEvent.MOUSE_BUTTON and not evt.is_pressed()):
+		water_anim.play("fall")
 		emit_signal("watering_can_clicked")
 	
