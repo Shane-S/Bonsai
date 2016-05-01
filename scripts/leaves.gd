2e6f24204fa06d5922f06e1f8bbe5941c74bfe73
brian <brian@copypants.com>
2016-05-01 01:52:57 -0700

    made the leaves script cllumps editable in editor

--- scripts/leaves.gd
+++ scripts/leaves.gd
@@ -2,12 +2,12 @@
 extends Sprite
 
 const COLOURS = ["green", "red", "blue", "pink"]
-const CLUMPS = 4
+export(int) var clumps = 4
 var moisture_zone_adjust = 0
 var light_zone_adjust = 0
 
 func _ready():
-	for i in range(CLUMPS):
+	for i in range(clumps):
 		var colour = randi() % 4
 		get_node("LeafClump" + str(i+1)).initialize(COLOURS[colour])
 	
@@ -22,13 +22,13 @@ func _ready():
 			light_zone_adjust -= 1.2
 
 func make_leaves_healthy():
-	for i in range(CLUMPS):
+	for i in range(clumps):
 		get_node("LeafClump" + str(i+1)).restore()
 
 func make_leaves_icy():
-	for i in range(CLUMPS):
+	for i in range(clumps):
 		get_node("LeafClump" + str(i+1)).make_icy()
 	
 func make_leaves_brown():
-	for i in range(CLUMPS):
+	for i in range(clumps):
 		get_node("LeafClump" + str(i+1)).make_brown()
